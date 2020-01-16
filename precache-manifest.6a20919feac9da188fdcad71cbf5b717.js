6a9afda47796dbaf49413ed20e3e77cffd2a7dce
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-01-16 14:11:05 +0200

    Updates

---
+++ precache-manifest.6a20919feac9da188fdcad71cbf5b717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294b7d0f358cc5a48aa987c3a6f92bc6",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "4e6dbe6b75ce4eaa5a00",
+    "url": "/react_uber-eats/static/css/main.1097f0a0.chunk.css"
+  },
+  {
+    "revision": "303853b0414359b41e8f",
+    "url": "/react_uber-eats/static/js/2.5e2875a3.chunk.js"
+  },
+  {
+    "revision": "4e6dbe6b75ce4eaa5a00",
+    "url": "/react_uber-eats/static/js/main.c588a58a.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.6a20919feac9da188fdcad71cbf5b717.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_uber-eats/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
